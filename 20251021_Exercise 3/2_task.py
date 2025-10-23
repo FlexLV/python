8603a71c11d20f11e9f0aa78c6dde9be5f6929e0
FlexLV <ernisd621@gmail.com>
2025-10-23 22:25:02 +0200

    Little misstypo

--- 20251021_Exercise 3/2_task.py
+++ 20251021_Exercise 3/2_task.py
@@ -1,4 +1,4 @@
 
 colors = ("red", "green", "blue")
 print(colors[1])
-# colors[1] = "yellow" cant change cause its tuple not list , set or dict its not mimmutable object
+# colors[1] = "yellow"  # can't change because tuple is immutable (unlike list, set, or dict)
